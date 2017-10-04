cdeca01acc64962ff7718646fc4aeca00f29d9c4
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-04 16:07:11 +0530

    second

--- app/src/main/java/com/jskgmail/assignmentapp/MainActivity.java
+++ app/src/main/java/com/jskgmail/assignmentapp/MainActivity.java
@@ -11,8 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -26,9 +33,28 @@ public class MainActivity extends AppCompatActivity
 
 
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
+        myRef.setValue("Hello, World!");
+// Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String TAG="sskkk";
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
 
-
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                String TAG="sjjs";
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
 HorizontalPhotosAdapter  horizontalAdapter;
 
